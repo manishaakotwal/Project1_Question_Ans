268b5df53b49c269fdc7e797d7662da3bd5119e3
Manisha <manishakotwal502@gmail.com>
2025-04-02 19:11:14 +0530

    some changes

--- mysite_app/admin.py
+++ mysite_app/admin.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from . models import Question, Choice
 
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("question_text", "pub_date")
+admin.site.register(Question)
+admin.site.register(Choice)
+
+# @admin.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     list_display = ("question_text", "pub_date")
     
-@admin.register(Choice)
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ("choice_text", "votes")    
+# @admin.register(Choice)
+# class ChoiceAdmin(admin.ModelAdmin):
+#     list_display = ("choice_text", "votes")    
     
--- mysite_app/models.py
+++ mysite_app/models.py
@@ -1,14 +1,25 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # two models: Questions- text, pub_date : Choice- text, votes
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("datee published")
+    
+    def __str__(self):
+        return self.question_text+"test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
 class Choice(models.Model):
     # on_delete = models.cascade means that if record in parent table is deleted then all related records in child table automatically deleted. 
     question = models.ForeignKey(Question, on_delete=models.CASCADE) 
     choice_text = models.CharField(max_length=200) 
-    votes = models.IntegerField(default=0)  
+    votes = models.IntegerField(default=0) 
+    
+    def __str__(self):
+        return self.question.question_text
